0ff98c7b3d90bd977ffe0022549b90711a45350e
Txuki <txukiakari@gmail.com>
2022-07-10 21:09:44 -0700

    Implement menu item to set objects on navmesh layer active or inactive

---
+++ Assets/Scripts/Utils/LayersEnum.cs
@@ -0,0 +1,12 @@
+namespace Moonlight
+{
+    public enum Layers
+    {
+        Default = 0,
+        TransparentFX = 1,
+        IgnoreRaycast = 2,
+        NavmeshGeneration = 3,
+        Water = 4,
+        UI = 5,
+    }
+}
