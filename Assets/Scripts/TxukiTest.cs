a7857995a65e7d0a803728077f432d3f359fc230
Txuki <txukiakari@gmail.com>
2022-07-25 23:12:25 -0700

    Add test script

---
+++ Assets/Scripts/TxukiTest.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class TxukiTest : MonoBehaviour
+{
+    [ContextMenu("Testing Method")]
+    public void TestingMethod()
+    {
+        GameObject go = new GameObject();
+
+    }
+}
